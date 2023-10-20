8b9a2e687d5f2ac1cccbd6fae0f86b1f73ad0f44
Isma <ismabarboza@gmail.com>
2023-10-20 12:05:47 -0300

    Confirm password component

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -28,12 +28,12 @@ export default function ConfirmPassword() {
             <Head title="Confirm Password" />
 
             <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                This is a secure area of the application. Please confirm your password before continuing.
+                Por favor confirme sua senha antes de continuar.
             </div>
 
             <form onSubmit={submit}>
                 <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                    <InputLabel htmlFor="password" value="Senha" />
 
                     <TextInput
                         id="password"
@@ -50,7 +50,7 @@ export default function ConfirmPassword() {
 
                 <div className="flex items-center justify-end mt-4">
                     <PrimaryButton className="ml-4" disabled={processing}>
-                        Confirm
+                        Confirmar
                     </PrimaryButton>
                 </div>
             </form>
