c0f449833182144811048b0d3fc4c5a520501490
Isma <ismabarboza@gmail.com>
2023-10-28 07:54:13 -0300

    Adicionado rotas de corpo de conhecimento

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CorpoConhecimentoController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('/corpo_conhecimento/create', [CorpoConhecimentoController::class, 'create'])->name('corpo_conhecimento.create');
+    Route::post('/corpo_conhecimento', [CorpoConhecimentoController::class, 'store'])->name('corpo_conhecimento.store');
 });
 
 require __DIR__.'/auth.php';
