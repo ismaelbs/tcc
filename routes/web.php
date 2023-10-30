2799b7abfa8887b9dccfeec05bdb96b1f2a0a458
Isma <ismabarboza@gmail.com>
2023-10-30 09:35:34 -0300

    Adicionado arquivo para rotas de corpo de conhecimento

--- routes/web.php
+++ routes/web.php
@@ -36,12 +36,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth', 'verified'])->group(function() {
-    Route::get('/corpo_conhecimento/create', [CorpoConhecimentoController::class, 'create'])->name('corpo_conhecimento.create');
-    Route::post('/corpo_conhecimento', [CorpoConhecimentoController::class, 'store'])->name('corpo_conhecimento.store');
-    Route::patch('/corpo_conhecimento/edit/{corpoConhecimento}', [CorpoConhecimentoController::class, 'update'])->name('corpo_conhecimento.update');
-    Route::patch('/corpo_conhecimento/disable/{corpoConhecimento}', [CorpoConhecimentoController::class, 'disable'])->name('corpo_conhecimento.disable');
-    Route::patch('/corpo_conhecimento/enable/{corpoConhecimento}', [CorpoConhecimentoController::class, 'enable'])->name('corpo_conhecimento.enable');
-});
-
+require __DIR__.'/corpo_conhecimento.php';
 require __DIR__.'/auth.php';
