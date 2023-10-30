4b3d3a39c076fd0ca07bf2312bc081fa2b4e68a3
Isma <ismabarboza@gmail.com>
2023-10-30 14:17:03 -0300

    Modificado rota para incluir corpo de conhecimento

--- routes/disciplina.php
+++ routes/disciplina.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\DisciplinaController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/disciplina/create', [DisciplinaController::class, 'create'])->name('disciplina.create');
+    Route::get('/{corpoConhecimento}/disciplina/create', [DisciplinaController::class, 'create'])->name('disciplina.create');
     Route::post('/disciplina', [DisciplinaController::class, 'store'])->name('disciplina.store');
     Route::patch('/disciplina/edit/{disciplina}', [DisciplinaController::class, 'update'])->name('disciplina.update');
     Route::patch('/disciplina/disable/{disciplina}', [DisciplinaController::class, 'disable'])->name('disciplina.disable');
