ab2d426bf57acfa6e07428285876d6bed19955f4
Isma <ismabarboza@gmail.com>
2023-11-06 15:06:27 -0300

    Adicionado gap na lista

--- resources/js/Pages/Questao/Partials/List.jsx
+++ resources/js/Pages/Questao/Partials/List.jsx
@@ -3,7 +3,7 @@ import Item from "./Item";
 export default function List({ questoes }) {
     return (<>
         {questoes.length === 0 && (<p className="mt-1 text-center text-sm text-gray-600 dark:text-gray-400">Nenhuma questão cadastrada no momento.</p>)}
-        <ul className="grid grid-cols-1">
+        <ul className="grid grid-cols-1 gap-2">
             {questoes.map(questao => (
                 <Item key={questao.id} questao={questao} />
             ))}
